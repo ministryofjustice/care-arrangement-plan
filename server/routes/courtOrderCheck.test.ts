32aa356b4b75c0af4b1b376bca08185846d4a003
Joseph Silcock <joseph.silcock@softwire.com>
2025-02-21 15:56:38 +0000

    C5141-760: Add tests

---
+++ server/routes/courtOrderCheck.test.ts
@@ -0,0 +1,41 @@
+import request from 'supertest'
+import testAppSetup from '../test-utils/testAppSetup'
+import paths from '../constants/paths'
+import formFields from '../constants/formFields'
+
+const app = testAppSetup()
+
+describe('/court-order-check', () => {
+  describe('GET', () => {
+    it('should render court order check order page', () => {
+      return request(app)
+        .get(paths.COURT_ORDER_CHECK)
+        .expect('Content-Type', /html/)
+        .expect(response => {
+          expect(response.text).toContain('Do you already have a court order in place about your child arrangements?')
+        })
+    })
+  })
+
+  describe('POST', () => {
+    it('should reload page when there is no body', () => {
+      return request(app).post(paths.COURT_ORDER_CHECK).expect(302).expect('location', paths.COURT_ORDER_CHECK)
+    })
+
+    it('should redirect to existing court order page if the answer is yes', () => {
+      return request(app)
+        .post(paths.COURT_ORDER_CHECK)
+        .send({ [formFields.COURT_ORDER_CHECK]: 'Yes' })
+        .expect(302)
+        .expect('location', paths.EXISTING_COURT_ORDER)
+    })
+
+    it('should redirect to number of children page if the answer is no', () => {
+      return request(app)
+        .post(paths.COURT_ORDER_CHECK)
+        .send({ [formFields.COURT_ORDER_CHECK]: 'No' })
+        .expect(302)
+        .expect('location', paths.START)
+    })
+  })
+})
---
+++ server/routes/existingCourtOrder.test.ts
@@ -0,0 +1,16 @@
+import request from 'supertest'
+import testAppSetup from '../test-utils/testAppSetup'
+import paths from '../constants/paths'
+
+const app = testAppSetup()
+
+describe('GET /existing-court-order', () => {
+  it('should render existing court order page', () => {
+    return request(app)
+      .get(paths.EXISTING_COURT_ORDER)
+      .expect('Content-Type', /html/)
+      .expect(response => {
+        expect(response.text).toContain('You can still use this service')
+      })
+  })
+})
