33c919148f17da42df00b3937b66959acc74cc02
Joseph Silcock <joseph.silcock@softwire.com>
2025-03-11 12:14:49 +0000

    C5141-1150: Add what happens next

--- server/pdf/createPdf.ts
+++ server/pdf/createPdf.ts
@@ -253,6 +253,34 @@ const createPdf = (autoPrint: boolean, request: Request) => {
     'If you do not agree, suggest what should happen about overnight visits',
     true,
   )
+
+  new TextComponent(pdf, [
+    {
+      text: i18n.__('sharePlan.yourProposedPlan.whatHappensNowHeading'),
+      size: SECTION_HEADING_SIZE,
+      style: 'bold',
+      bottomPadding: PARAGRAPH_SPACE,
+    },
+    {
+      text: i18n.__('sharePlan.yourProposedPlan.nowSendPlan', { senderName: request.session.initialAdultName }),
+      size: MAIN_TEXT_SIZE,
+      style: 'normal',
+      bottomPadding: PARAGRAPH_SPACE,
+    },
+    {
+      text: i18n.__('sharePlan.yourProposedPlan.unableToAgree'),
+      size: MAIN_TEXT_SIZE,
+      style: 'normal',
+      bottomPadding: PARAGRAPH_SPACE,
+    },
+    {
+      text: i18n.__('sharePlan.yourProposedPlan.moreInfoAndSupport'),
+      size: MAIN_TEXT_SIZE,
+      style: 'normal',
+      bottomPadding: PARAGRAPH_SPACE,
+    },
+  ]).addComponentToDocument()
+
   pdf.addFooterToEveryPage()
 
   return pdf.toArrayBuffer()
