0103d7d4b16b175c5d4f48bdb0bd43a8562cd6e0
Joseph Silcock <joseph.silcock@softwire.com>
2025-03-11 13:55:54 +0000

    C5141-1150: Add about the proposal

--- server/pdf/createPdf.ts
+++ server/pdf/createPdf.ts
@@ -3,12 +3,15 @@ import Pdf from './pdf'
 import addPreamble from './addPreamble'
 import addWhatHappensNow from './addWhatHappensNow'
 import addSpecialDays from './addSpecialDays'
+import addAboutTheProposal from './addAboutTheProposal'
 
 const createPdf = (autoPrint: boolean, request: Request) => {
   const pdf = new Pdf(autoPrint)
 
   addPreamble(pdf, request)
   pdf.createNewPage()
+
+  addAboutTheProposal(pdf, request)
 
   addSpecialDays(pdf, request)
 
