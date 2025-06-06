70bb75bd12d24f18e7a0bbbb5d40ebc8fb767020
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2025-06-06 15:27:25 +0100

    Update setupRateLimit.ts

--- server/middleware/setupRateLimit.ts
+++ server/middleware/setupRateLimit.ts
@@ -16,7 +16,7 @@ const setupRateLimit = () => {
 
   return rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per windowMs
+    max: 250, // Limit each IP to 250 requests per windowMs
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
     validate: { trustProxy: false }, // Don't warn about trusting the proxy
