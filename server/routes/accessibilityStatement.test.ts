5cbe2d1cd1fba676ba076ad626c03895cf04cfc9
nina-knowles-justice <nina.knowles@justice.gov.uk>
2025-10-29 16:01:34 +0000

    DAS-718 Test logging

--- server/routes/accessibilityStatement.test.ts
+++ server/routes/accessibilityStatement.test.ts
@@ -3,12 +3,14 @@ import request from 'supertest';
 
 import paths from '../constants/paths';
 import testAppSetup from '../test-utils/testAppSetup';
+import logger from '../logger';
 
 const app = testAppSetup();
 
 describe(paths.ACCESSIBILITY_STATEMENT, () => {
   describe('GET', () => {
     it('should render accessibility statement page', async () => {
+      logger.info(`GET ${paths.ACCESSIBILITY_STATEMENT}`);
       const response = await request(app).get(paths.ACCESSIBILITY_STATEMENT).expect('Content-Type', /html/);
 
       const dom = new JSDOM(response.text);
