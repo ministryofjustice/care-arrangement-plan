8676735e6e7c53f7c02d5b991c782d2cde2de15e
nina-knowles-justice <nina.knowles@justice.gov.uk>
2025-11-18 14:07:36 +0000

    DAS-820 Remove non page visits from user events

--- server/logging/setupPageVisitAnalytics.ts
+++ server/logging/setupPageVisitAnalytics.ts
@@ -3,7 +3,16 @@ import onFinished from 'on-finished';
 
 import { logPageVisit } from '../services/analyticsService';
 
-const EXCLUDED_PATHS = ['/health', '/ping'];
+const EXCLUDED_PATHS = ['/health', '/ping', '/manifest.json' ];
+
+const EXCLUDED_PATTERNS = [
+  /^\/assets/,
+  /^\/images/,
+  /^\/js/,
+  /^\/fonts/,
+  /^\/css/
+];
+
 
 /**
  * Middleware to set up page visit analytics logging.
@@ -14,11 +23,15 @@ const setupPageVisitAnalytics = () => {
       const { method, path } = req;
       const { statusCode: responseStatusCode } = res;
 
-      if (method !== 'GET' || responseStatusCode >= 400) {
+      if (method !== 'GET' || responseStatusCode >= 400 ) {
         return;
       }
 
       if (EXCLUDED_PATHS.includes(path)) {
+        return;
+      }
+
+      if (EXCLUDED_PATTERNS.some(pattern => pattern.test(path))) {
         return;
       }
 
