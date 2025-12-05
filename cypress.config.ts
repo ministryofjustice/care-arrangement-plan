1e18aa8708bbc799f72c8b2c8104be3ee6b5bf1e
ismail-walsh-justice <ismail.walsh@justice.gov.uk>
2025-12-05 10:20:44 +0000

    feat: cypress test suite added

--- cypress.config.ts
+++ cypress.config.ts
@@ -10,10 +10,13 @@ export default defineConfig({
     configFile: 'reporter-config.json',
   },
   taskTimeout: 60000,
+  env: {
+    BETA_ACCESS_PASSWORD: process.env.CYPRESS_BETA_ACCESS_PASSWORD || 'parent-planner',
+  },
   e2e: {
     baseUrl: 'http://localhost:8001',
     excludeSpecPattern: '**/!(*.cy).ts',
     specPattern: 'integration-tests/e2e/**/*.cy.{js,jsx,ts,tsx}',
-    supportFile: false,
+    supportFile: 'integration-tests/support/e2e.ts',
   },
 });
