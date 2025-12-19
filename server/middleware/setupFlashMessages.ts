d984f926491be3d44b24928db89f8bd4aedc1386
nina-knowles-justice <nina.knowles@justice.gov.uk>
2025-12-19 11:01:59 +0000

    das-2016 remove error flash

--- server/middleware/setupFlashMessages.ts
+++ server/middleware/setupFlashMessages.ts
@@ -3,7 +3,6 @@ import { Request, Response, NextFunction } from 'express';
 const setupFlashMessages = () => {
   return (req: Request, res: Response, next: NextFunction) => {
     res.locals.info = req.flash('info');
-    res.locals.errors = req.flash('errors');
     next();
   };
 };
