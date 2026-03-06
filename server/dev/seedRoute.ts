6577a37d09646674938f4a97ba873103eba82419
nina-knowles-justice <nina.knowles@justice.gov.uk>
2026-03-06 11:28:57 +0000

    das-1689 pre fill form data in dev

---
+++ server/dev/seedRoute.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+
+import FORM_STEPS from '../constants/formSteps';
+import paths from '../constants/paths';
+
+import devSeedData from './seedData';
+
+const devSeedRoutes = (router: Router) => {
+  router.get('/dev/seed', (request, response) => {
+    Object.assign(request.session, devSeedData, { planStartTime: Date.now() });
+
+    request.session.completedSteps = Object.values(FORM_STEPS);
+    request.session.pageHistory = [paths.START];
+
+    const redirect = request.query.redirect as string;
+    const target = redirect && redirect.startsWith('/') ? redirect : paths.TASK_LIST;
+
+    response.redirect(target);
+  });
+};
+
+export default devSeedRoutes;
