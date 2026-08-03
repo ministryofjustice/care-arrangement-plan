261f49105b3859622f6df8b320ae22b269581b3a
john-benjamin-justice <john.benjamin@justice.gov.uk>
2026-08-03 17:27:34 +0100

    DAS-2334 Update page title (#363)

--- e2e-tests/task-list.spec.ts
+++ e2e-tests/task-list.spec.ts
@@ -39,6 +39,26 @@ test.describe('Task List', () => {
     await expect(taskListHeading).toBeVisible();
   });
 
+  test('should set the browser page title for the task list', async ({ page }) => {
+    await expect(page).toHaveURL(/\/do-whats-best/);
+
+    // Continue through the flow until the task list is reached
+    await page.getByRole('checkbox', { name: /I will put my children.s needs first/i }).check();
+    await page.getByRole('button', { name: /continue/i }).click();
+    await page.getByLabel(/no/i).first().check();
+    await page.getByRole('button', { name: /continue/i }).click();
+    await page.getByLabel(/How many children is this for/i).fill('1');
+    await page.getByRole('button', { name: /continue/i }).click();
+    await page.fill('input[name="child-name0"]', 'Test');
+    await page.getByRole('button', { name: /continue/i }).click();
+    await page.fill('input[name="initial-adult-name"]', 'Parent');
+    await page.fill('input[name="secondary-adult-name"]', 'Guardian');
+    await page.getByRole('button', { name: /continue/i }).click();
+
+    await expect(page).toHaveURL(/\/make-a-plan/);
+    await expect(page).toHaveTitle(/Child arrangement plan with child\(ren\) details/);
+  });
+
   test('should show task list sections', async ({ page }) => {
     await page.goto('/make-a-plan');
 
