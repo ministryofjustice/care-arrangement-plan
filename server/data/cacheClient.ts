fdfa8f20c4027834ba3b5358a3ae4767b0f43a1c
john-benjamin-justice <john.benjamin@justice.gov.uk>
2026-09-14 10:33:49 +0100

    DAS-2374 Fix code scanning alerts

--- server/data/cacheClient.ts
+++ server/data/cacheClient.ts
@@ -5,20 +5,26 @@ import logger from '../logging/logger';
 
 type CacheClient = ReturnType<typeof createClient>;
 
-const url = `${config.cache.tls_enabled ? 'rediss' : 'redis'}://${config.cache.host}`;
+const reconnectStrategy = (attempts: number) => {
+  // Exponential back off: 20ms, 40ms, 80ms..., capped to retry every 30 seconds
+  const nextDelay = Math.min(2 ** attempts * 20, 30000);
+  logger.info(`Retry cache connection attempt: ${attempts}, next attempt in: ${nextDelay}ms`);
+  return nextDelay;
+};
 
 const createCacheClient = (): CacheClient => {
   const client = createClient({
-    url,
     password: config.cache.password,
-    socket: {
-      reconnectStrategy: (attempts: number) => {
-        // Exponential back off: 20ms, 40ms, 80ms..., capped to retry every 30 seconds
-        const nextDelay = Math.min(2 ** attempts * 20, 30000);
-        logger.info(`Retry cache connection attempt: ${attempts}, next attempt in: ${nextDelay}ms`);
-        return nextDelay;
-      },
-    },
+    socket: config.cache.tls_enabled
+      ? {
+          host: config.cache.host,
+          tls: true,
+          reconnectStrategy,
+        }
+      : {
+          host: config.cache.host,
+          reconnectStrategy,
+        },
   });
 
   client.on('error', (e: Error) => logger.error('Cache client error', e));
