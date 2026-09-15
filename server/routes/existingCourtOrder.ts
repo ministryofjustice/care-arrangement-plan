9e2385789b53a752e4f22f073321efa08accc335
john-benjamin-justice <john.benjamin@justice.gov.uk>
2026-09-15 10:27:32 +0100

    DAS-2101 welsh main user journey additional changes (#381)
    * DAS-2101 Welsh additional changes after review
    
    * DAS-2101 Welsh main user journey
    
    * DAS-2290 Welsh translation recipient PDF
    
    * DAS-2101 Welsh additional changes after review

--- server/routes/existingCourtOrder.ts
+++ server/routes/existingCourtOrder.ts
@@ -2,12 +2,14 @@ import { Router } from 'express';
 
 import FORM_STEPS from '../constants/formSteps';
 import paths from '../constants/paths';
-import checkFormProgressFromConfig  from '../middleware/checkFormProgressFromConfig';
+import checkFormProgressFromConfig from '../middleware/checkFormProgressFromConfig';
+import { getBackUrl } from '../utils/sessionHelpers';
 
 const existingCourtOrderRoutes = (router: Router) => {
   router.get(paths.EXISTING_COURT_ORDER, checkFormProgressFromConfig(FORM_STEPS.EXISTING_COURT_ORDER), (request, response) => {
     response.render('pages/existingCourtOrder', {
       title: request.__('existingCourtOrder.title'),
+      backLinkHref: getBackUrl(request.session, paths.COURT_ORDER_CHECK),
     });
   });
 };
